8ce469a5b76baa097c23b894d601d82ccd5b6bb4
Jonathan Gostelow <jonathan.gostelow@gmail.com>
2016-03-09 22:03:41 -0600

    Consolidated environments

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ ln -s $basedir/.screenrc ~/
 ln -s $basedir/.vim ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/wincent/Command-T.git ~/.vim/bundle/Command-T
 ln -s $basedir/.vimrc ~/
