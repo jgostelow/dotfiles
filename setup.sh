7406ad6a85e677430920c56cd54cf16b033c4e35
Jonathan Gostelow <jonathan.gostelow@gmail.com>
2016-03-11 21:35:43 -0600

    Remove git repos from .vim bundles

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-basedir=$(pwd)/base
+basedir=$(pwd)
 
-ln -s $basedir/.aliases ~/
-ln -s $basedir/.bash_profile ~/
-ln -s $basedir/.gitconfig ~/
-ln -s $basedir/.screenrc ~/
-ln -s $basedir/.vim ~/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-git clone https://github.com/wincent/Command-T.git ~/.vim/bundle/Command-T
-ln -s $basedir/.vimrc ~/
+ln -sf $basedir/.aliases ~/
+ln -sf $basedir/.bash_profile ~/
+ln -sf $basedir/.gitconfig.personal ~/.gitconfig
+ln -sf $basedir/.screenrc ~/
+ln -sf $basedir/.vim ~/
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+#git clone https://github.com/wincent/Command-T.git ~/.vim/bundle/Command-T
+ln -sf $basedir/.vimrc ~/
