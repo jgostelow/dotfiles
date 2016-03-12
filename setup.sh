923b1a9068c916dd61ec62dfe576f61e7ecfac7d
Jonathan Gostelow <jonathan.gostelow@gmail.com>
2016-03-11 21:51:28 -0600

    Remove vim plugin git repos from setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,4 @@ ln -sf $basedir/.bash_profile ~/
 ln -sf $basedir/.gitconfig.personal ~/.gitconfig
 ln -sf $basedir/.screenrc ~/
 ln -sf $basedir/.vim ~/
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-#git clone https://github.com/wincent/Command-T.git ~/.vim/bundle/Command-T
 ln -sf $basedir/.vimrc ~/
