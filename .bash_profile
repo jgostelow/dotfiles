4c3c9970297a4217458f8e4a413f0eddd169c64b
Jonathan Gostelow <jgostelow@groupon.com>
2016-03-31 10:21:42 -0500

    Update 2016-03-03

--- .bash_profile
+++ .bash_profile
@@ -17,3 +17,5 @@ PS1="\[\e[01;32m\]\u@\h\[\e[m\]:\[\e[01;34m\]\t\[\e[m\]:\[\e[01;31m\]\w\[\e[m\]\
 
 #set -o vi
 source $HOME/.aliases
+
+export PATH="~/.rbenv/shims:$PATH"
--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@ basedir=$(pwd)
 
 ln -sf $basedir/.aliases ~/
 ln -sf $basedir/.bash_profile ~/
-ln -sf $basedir/.gitconfig.personal ~/.gitconfig
+ln -sf $basedir/.gitconfig.groupon ~/.gitconfig
 ln -sf $basedir/.screenrc ~/
 ln -sf $basedir/.vim ~/
 ln -sf $basedir/.vimrc ~/
+git submodule init
+git submodule update
