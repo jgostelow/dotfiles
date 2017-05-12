7604fa59c9468aece1978e586082edd43b372350
Jonathan Gostelow <jonathan.gostelow@gmail.com>
2017-05-12 11:41:54 -0500

    Slim down setup scripts and move to OS specific directories

---
+++ linux/setup.linux.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+basedir=$(pwd)
+
+ENV=$1    # personal,wyzant
+
+function install {
+  if ! command -v $1 > /dev/null; then
+    echo "###### Installing $1 ######"
+    sudo apt-get install -y $1
+  fi
+}
+
+### GENERAL ###
+echo "Installing some basic things......"
+sudo apt-get update
+install 'wget'
+install 'ack-grep'
+
+echo "Setting up aliases......"
+echo "source $basedir/aliases" > ~/.aliases
+echo "source $basedir/linux/aliases" >> ~/.aliases
+
+### GIT ###
+echo "Setting up git config......"
+cat > ~/.gitconfig << EOF
+[include]
+  path = $basedir/gitconfig
+EOF
+if [ -n "$ENV" ] && [[ -r $basedir/gitconfig.$ENV ]]; then
+  cat >> ~/.gitconfig << EOF
+  path = $basedir/gitconfig.$ENV
+EOF
+fi
+ln -sf $basedir/.gitignore_global ~/
+
+### VIM ###
+echo "Setting up vim......"
+ln -sf $basedir/.vim ~/
+ln -sf $basedir/.vimrc ~/
+git submodule init
+git submodule update
+
+### MISC ###
+echo "Setting up screen and bin directory......"
+ln -sf $basedir/.screenrc ~/
+ln -sf $basedir/bin ~/
+
+### ZSH ###
+#install 'zsh-antigen'
+echo "Installing and switching to ZSH......"
+install 'zsh'
+chsh -s `which zsh`
+curl -L git.io/antigen > ~/antigen.zsh
+echo "source $basedir/zshrc" > ~/.zshrc
+echo "source $basedir/env" >> ~/.zshrc
+/usr/bin/zsh -i -c "source ~/antigen.zsh"
+
+echo "Linux Setup complete!"
---
+++ osx/setup.osx.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+basedir=$(pwd)
+
+ENV=$1    # personal,wyzant
+
+function install {
+  if ! command -v $1 > /dev/null; then
+    echo "###### Installing $1 ######"
+    brew install $1
+  fi
+}
+
+### GENERAL ###
+echo "Installing Homebrew and some basic things......"
+if [ ! type "brew" > /dev/null ]; then # Homebrew (osx)
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+brew update
+install 'wget'
+install 'ack'
+
+echo "source $basedir/aliases" > ~/.aliases
+echo "source $basedir/osx/aliases" >> ~/.aliases
+
+### GIT ###
+echo "Setting up git config......"
+cat > ~/.gitconfig << EOF
+[include]
+  path = $basedir/gitconfig
+EOF
+if [ -n "$ENV" ] && [[ -r $basedir/gitconfig.$ENV ]]; then
+  cat >> ~/.gitconfig << EOF
+  path = $basedir/gitconfig.$ENV
+EOF
+fi
+ln -sf $basedir/.gitignore_global ~/
+install 'hub'
+
+### VIM ###
+echo "Setting up vim......"
+ln -sf $basedir/.vim ~/
+ln -sf $basedir/.vimrc ~/
+git submodule init
+git submodule update
+
+### MISC ###
+echo "Setting up screen and bin directory......"
+ln -sf $basedir/.screenrc ~/
+ln -sf $basedir/bin ~/
+
+### ZSH ###
+echo "Installing and switching to ZSH......"
+install 'zsh'
+chsh -s `which zsh`
+curl -L git.io/antigen > ~/antigen.zsh
+echo "source $basedir/zshrc" > ~/.zshrc
+echo "source $basedir/env" >> ~/.zshrc
+/usr/bin/zsh -i -c "source ~/antigen.zsh"
+
+echo "OSX Setup complete!"
