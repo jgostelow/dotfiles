58abaa059e33f57180c2616708e3f3cde5ed0bf9
Jonathan Gostelow <jonathan.gostelow@gmail.com>
2022-11-12 16:39:56 -0600

    Updated wsl setup

--- linux/setup.sh
+++ linux/setup.sh
@@ -2,43 +2,59 @@
 
 basedir=$HOME/GIT/dotfiles
 
+function binstall {
+  if ! command -v $1 > /dev/null; then
+    echo "###### Installing (homebrew) $1 ######"
+    brew install $1
+  fi
+}
+
 function install {
   if ! command -v $1 > /dev/null; then
-    echo "###### Installing $1 ######"
-    brew install $1
+    echo "###### Installing (apt) $1 ######"
+    sudo apt install $1
   fi
 }
 
 ### GENERAL ###
 echo "###### Installing homebrew"
-[ ! -f "`which brew`" ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [ ! -f "`which brew`" ]; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    	echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/raziel/.profile
+    	echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/raziel/.profile
+    	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    	sudo apt-get install build-essential
+fi
 brew update > /dev/null
-install 'vim'
-install 'wget'
-install 'glances'
-install 'tig'
-install 'jq'
-install 'yq'
-install 'tmux'
-install 'ctags'
-install 'vifm'
-install 'ripgrep'
-install 'moreutils' # http://joeyh.name/code/moreutils/
+binstall 'vim'
+binstall 'wget'
+binstall 'glances'
+binstall 'watch'
+binstall 'tig'
+binstall 'jq'
+binstall 'yq'
+binstall 'tmux'
+binstall 'ctags'
+binstall 'vifm'
+binstall 'ripgrep'
+binstall 'moreutils' # http://joeyh.name/code/moreutils/
+binstall 'openssl'
+sudo apt install 'libz-dev'
 
 # https://www.vimfromscratch.com/articles/awesome-command-line-tools/
-install 'tldr'
-install 'bat'
-install 'exa'
-install 'fd'
-install 'fzf'
+binstall 'tldr'
+binstall 'bat'
+binstall 'exa'
+binstall 'fd'
+binstall 'fzf'
 
-install 'jandedobbeleer/oh-my-posh/oh-my-posh' # https://ohmyposh.dev/
-install 'nodejs' # required by coc.vim
+binstall 'jandedobbeleer/oh-my-posh/oh-my-posh' # https://ohmyposh.dev/
+binstall 'nodejs' # required by coc.vim
 
 brew tap jesseduffield/lazydocker
-install 'lazydocker'
+binstall 'lazydocker'
 brew tap jesseduffield/lazygit
-install 'lazygit'
+binstall 'lazygit'
 ln -sf $basedir/base/lazygit.config.yml ~/.config/lazygit/config.yml
 
 echo "Setting up aliases......"
@@ -50,7 +66,7 @@ cat > ~/.gitconfig << EOF
 [include]
   path = $basedir/base/gitconfig
 EOF
-install 'diff-so-fancy'
+binstall 'diff-so-fancy'
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 ln -sf $basedir/base/.gitignore_global ~/
 
@@ -71,13 +87,13 @@ echo "Installing to zsh + oh-my-zsh"
 install 'zsh'
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s `which zsh`
-install antigen
+binstall antigen
 echo "source $basedir/zsh/zshrc" > ~/.zshrc
 echo "source $baserdir/zsh/functions.zsh" >> ~/.zshrc
 
 ### Ruby ###
-install 'rbenv'
-install 'ruby-build'
+binstall 'rbenv'
+binstall 'ruby-build'
 
 echo "#### Installing Ruby 2.7 ####"
 rbenv install 2.7.4
@@ -89,4 +105,4 @@ ln -sf $basedir/base/.gemrc ~/
 echo "#### Installing Rails 5.2 ####"
 gem install rails -v 6.1.6
 
-echo "Windows Setup complete!"
+echo "Linux Setup complete!"
