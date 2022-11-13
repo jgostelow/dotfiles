e2bf667394585490642a9818e0ea3cef1ad1909f
Jonathan Gostelow <jonathan.gostelow@gmail.com>
2022-11-12 20:01:16 -0600

    Update linux setup

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-basedir=$HOME/GIT/dotfiles
+basedir=$HOME/dotfiles
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+HOMEBREW_NO_INSTALL_CLEANUP=1
 
 function binstall {
-  if ! command -v $1 > /dev/null; then
-    echo "###### Installing (homebrew) $1 ######"
-    brew install $1
-  fi
+	if ! command -v $1 > /dev/null; then
+		printf "${CYAN}############################################################## BREW: Installing $1${NC}\n"
+		brew install $1
+	fi
 }
 
 function install {
-  if ! command -v $1 > /dev/null; then
-    echo "###### Installing (apt) $1 ######"
-    sudo apt install $1
-  fi
+	if ! command -v $1 > /dev/null; then
+		printf "${CYAN}##############################################################  APT : Installing $1${NC}\n"
+		sudo apt install $1
+	fi
 }
 
 ### GENERAL ###
-echo "###### Installing homebrew"
+sudo apt update &> /dev/null
 if [ ! -f "`which brew`" ]; then
+	printf "${CYAN}############################################################## Installing homebrew${NC}\n"
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    	echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/raziel/.profile
-    	echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/raziel/.profile
-    	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    	sudo apt-get install build-essential
+	echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/raziel/.profile
+	echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/raziel/.profile
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	sudo apt-get install build-essential
 fi
 brew update > /dev/null
+
 binstall 'vim'
 binstall 'wget'
 binstall 'glances'
@@ -39,7 +44,7 @@ binstall 'vifm'
 binstall 'ripgrep'
 binstall 'moreutils' # http://joeyh.name/code/moreutils/
 binstall 'openssl'
-sudo apt install 'libz-dev'
+install 'libz-dev'
 
 # https://www.vimfromscratch.com/articles/awesome-command-line-tools/
 binstall 'tldr'
@@ -48,7 +53,6 @@ binstall 'exa'
 binstall 'fd'
 binstall 'fzf'
 
-binstall 'jandedobbeleer/oh-my-posh/oh-my-posh' # https://ohmyposh.dev/
 binstall 'nodejs' # required by coc.vim
 
 brew tap jesseduffield/lazydocker
@@ -57,11 +61,11 @@ brew tap jesseduffield/lazygit
 binstall 'lazygit'
 ln -sf $basedir/base/lazygit.config.yml ~/.config/lazygit/config.yml
 
-echo "Setting up aliases......"
+printf "${CYAN}############################################################## Setting up aliases......\n${NC}"
 echo "source $basedir/base/aliases" > ~/.aliases
 
 ### GIT ###
-echo "Setting up git config......"
+printf "${CYAN}############################################################## Setting up git config......\n${NC}"
 cat > ~/.gitconfig << EOF
 [include]
   path = $basedir/base/gitconfig
@@ -71,38 +75,40 @@ git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 ln -sf $basedir/base/.gitignore_global ~/
 
 ### VIM ###
-echo "Setting up vim......"
+printf "${CYAN}############################################################## Setting up vim......\n${NC}"
 ln -sf $basedir/base/.vim/* ~/.vim/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +'PlugInstall' +qa
 
 ### TMUX ###
-echo "Setting up tmux......"
+printf "${CYAN}############################################################## Setting up tmux......\n${NC}"
 ln -sf $basedir/base/.tmux.conf ~/
 echo "source-file ~/.tmux.conf" > ~/.tmate.conf
 tmux source ~/.tmux.conf
 
 ### ZSH ###
-echo "Installing to zsh + oh-my-zsh"
+printf "${CYAN}############################################################## Installing to zsh + oh-my-zsh \n${NC}"
 install 'zsh'
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s `which zsh`
-binstall antigen
+install zsh-antigen
 echo "source $basedir/zsh/zshrc" > ~/.zshrc
 echo "source $baserdir/zsh/functions.zsh" >> ~/.zshrc
+binstall 'jandedobbeleer/oh-my-posh/oh-my-posh' # https://ohmyposh.dev/
 
 ### Ruby ###
 binstall 'rbenv'
 binstall 'ruby-build'
 
-echo "#### Installing Ruby 2.7 ####"
+printf "${CYAN}############################################################## Installing Ruby 2.7 \n${NC}"
 rbenv install 2.7.4
 rbenv global 2.7.4
 rbenv rehash
 
 ln -sf $basedir/base/.gemrc ~/
 
-echo "#### Installing Rails 5.2 ####"
+printf "${CYAN}############################################################## Installing Rails 5.2 \n${NC}"
 gem install rails -v 6.1.6
 
-echo "Linux Setup complete!"
+printf "${CYAN}############################################################## Linux Setup complete! \n${NC}"
+printf "${CYAN}############################################################## Switching to ZSH \n${NC}"
+chsh -s `which zsh`
