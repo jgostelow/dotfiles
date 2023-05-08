96d225e898116edfb26d7da44d7fdd0c5cbd7fb9
Jonathan Gostelow <jonathan.gostelow@gmail.com>
2023-05-07 21:59:49 -0500

    WIP Docker dev env

---
+++ linux/setup-docker.sh
@@ -0,0 +1,30 @@
+#/usr/bin/bash
+
+# SOURCE : https://nickjanetakis.com/blog/install-docker-in-wsl-2-without-docker-desktop
+
+# Install Docker, you can ignore the warning from Docker about using WSL
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# Add your user to the Docker group
+sudo usermod -aG docker $USER
+
+# Install Docker Compose v2
+sudo apt-get update && sudo apt-get install docker-compose-plugin
+
+# Sanity check that both tools were installed successfully
+docker --version
+docker compose version
+
+# Using Ubuntu 22.04 or Debian 10 / 11? You need to do 1 extra step for iptables
+# compatibility, you'll want to choose option (1) from the prompt to use iptables-legacy.
+sudo update-alternatives --config iptables
+
+cat >> ~/.profile << EOF
+if grep -q "microsoft" /proc/version > /dev/null 2>&1; then
+    if service docker status 2>&1 | grep -q "is not running"; then
+        wsl.exe --distribution "${WSL_DISTRO_NAME}" --user root \
+            --exec /usr/sbin/service docker start > /dev/null 2>&1
+    fi
+fi
+EOF
