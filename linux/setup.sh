655807969974a32d6dadfcbfeb6cb332dc882cf5
Jonathan Gostelow <jonathan.gostelow@gmail.com>
2023-11-17 14:07:47 -0600

    install node for vim.coc

--- linux/setup.sh
+++ linux/setup.sh
@@ -76,10 +76,24 @@ function setup_git() {
   ln -sf $basedir/base/.gitignore_global ~/
 }
 
+function install_node() {
+  printf "${CYAN}############################################################## Installing v21.x Nodejs......\n${NC}"
+  install 'ca-certificates'
+  install 'curl'
+  install 'gnupg'
+  sudo mkdir -p /etc/apt/keyrings
+  NODE_MAJOR=21
+  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+  sudo apt-get update
+  install 'nodejs' # required by coc.vim
+
+}
+
 function setup_vim() {
   printf "${CYAN}############################################################## Setting up vim......\n${NC}"
   install 'vim'
-  ln -sf $basedir/base/.vim/* ~/.vim/
+  ln -sf $basedir/base/.vim ~/
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   vi +'PlugInstall' +qa
 }
@@ -139,9 +153,10 @@ install_homebrew
 install_packages
 setup_aliases
 setup_git
+install_node
 setup_vim
-setup_tmux
 setup_zsh
+setup_tmux
 setup_ruby
 cleanup
 printf "${CYAN}############################################################## Linux Setup complete! \n${NC}"
