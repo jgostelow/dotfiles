eeb15df45c199c6e41ccde581939bc7564f06344
Jonathan Gostelow <jonathan.gostelow@gmail.com>
2024-01-05 19:21:37 -0600

    Linux Python setup

--- linux/python.sh
+++ linux/python.sh
@@ -6,15 +6,22 @@ source $basedir/linux/functions.sh
 
 function setup_python() {
   binstall 'pyenv'
+  install 'python3-venv'
+  binstall 'pyenv-virtualenv'
 
   printf "${CYAN}############################################################## Installing Python\n${NC}"
-  eval "$(pyenv init -)"
   echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
   echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
   echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+  echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+  eval "$(pyenv init -)"
+  echo "$(pyenv virtualenv-init -)"
+
+  pyenv install 3.11
+  pyenv global 3.11
+  pyenv rehash
 
   printf "${CYAN}############################################################## Installing Django\n${NC}"
-  gem install rails -v 7.1.0
 }
 
 setup_python
